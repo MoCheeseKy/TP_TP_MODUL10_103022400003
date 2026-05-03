150b5cba22e534458b7d6cec784e8532e66065c0
Your NameMoCheeseKy <rifkyking04@gmail.com>
2026-05-04 01:24:27 +0700

    done bang

---
+++ TP_MODUL10_103022400003/Controllers/FilmController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_MODUL10_103022400003.Models;
+
+namespace TP_MODUL10_103022400003.Controllers
+{
+    [ApiController]
+    [Route("api/Film")]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> filmList = new List<Film>
+        {
+            new Film { Judul = "Inception", Sutradara = "Christopher Nolan", Tahun = "2010", Genre = "Sci-Fi", Rating = "9.0" },
+            new Film { Judul = "Interstellar", Sutradara = "Christopher Nolan", Tahun = "2014", Genre = "Sci-Fi", Rating = "8.7" },
+            new Film { Judul = "Parasite", Sutradara = "Bong Joon-ho", Tahun = "2019", Genre = "Thriller", Rating = "8.6" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Film>> GetAll()
+        {
+            return Ok(filmList);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Film> GetByIndex(int index)
+        {
+            if (index < 1 || index > filmList.Count)
+            {
+                return NotFound();
+            }
+            return Ok(filmList[index - 1]);
+        }
+
+        [HttpPost]
+        public ActionResult AddFilm([FromBody] Film film)
+        {
+            filmList.Add(film);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteFilm(int index)
+        {
+            if (index < 1 || index > filmList.Count)
+            {
+                return NotFound();
+            }
+            filmList.RemoveAt(index - 1);
+            return Ok();
+        }
+    }
+}
---
+++ TP_MODUL10_103022400003/Models/Film.cs
@@ -0,0 +1,15 @@
+﻿// bang, ini aku bungkus folder  models yak bar rapihan dikit codenya :'
+
+namespace TP_MODUL10_103022400003.Models
+{
+    public class Film
+    {
+        public string Judul { get; set; } = string.Empty;
+        public string Sutradara { get; set; } = string.Empty;
+        public string Tahun { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+
+        public Film() { }
+    }
+}
